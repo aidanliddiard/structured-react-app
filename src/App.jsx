dd94127b0d2bb9ba4399b2fdfdf24b15e7146ece
Mary Martinez <mary.diana.martinez@gmail.com>
2022-05-17 17:00:21 -0700

    basic routes set up to empty pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,25 @@
+import { Route, Switch } from 'react-router-dom';
+import Auth from './views/Auth';
+import DetailView from './views/DetailView';
+import Home from './views/Home';
+import ListView from './views/ListView';
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Switch>
+        <Route path={'/auth'}>
+          <Auth />
+        </Route>
+        <Route path={'/blogs/:id'}>
+          <DetailView />
+        </Route>
+        <Route path={'/blogs'}>
+          <ListView />
+        </Route>
+        <Route path={'/'}>
+          <Home />
+        </Route>
+      </Switch>
+    </>
+  );
 }
