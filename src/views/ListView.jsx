0c3c821c58cab886e18c47b4ed8ed44ed91bbf6b
Mary Martinez <mary.diana.martinez@gmail.com>
2022-05-17 17:13:29 -0700

    mock data rendering

--- src/views/ListView.jsx
+++ src/views/ListView.jsx
@@ -1,5 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import { blogData } from '../../MockBlogData';
 
 export default function ListView() {
-  return <div>ListView</div>;
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    setBlogs(blogData);
+  }, []);
+
+  return (
+    <>
+      <h1>Welcome to Hear 'n There</h1>
+      <h2>Previous Blogs</h2>
+      {blogs.map((blog) => (
+        <h3 key={blog.id}>{blog.title}</h3>
+      ))}
+    </>
+  );
 }
